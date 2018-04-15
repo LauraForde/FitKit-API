dfeb36a8e3ab67b4f50a625e1dfc434fc70b7c4c
LauraForde <G00315765@gmit.ie>
2018-04-15 23:24:00 +0100

    Database

--- CouchProvider.py
+++ CouchProvider.py
@@ -1,10 +1,24 @@
 import requests
-login = {
-    "_id":"1234",
-    "name":"Laura",
-    "password":"1234"
-}
+import couchdb
+ADMIN_USERNAME = 'admin'
+ADMIN_PASSWORD = 'pass'
+COUCHDB_URL = 'http://'+ADMIN_USERNAME+':'+ADMIN_PASSWORD+'@localhost:5984/'
+couch = couchdb.Server(COUCHDB_URL)
+
 
 class CouchProvider(object):
-    def read_login(self) -> str:
-        return login,1
+    def create_user(self,payload):
+        db = couch['login']
+        if payload['_id'] in db:
+            return {"error":"This username is taken!"},409
+        else:
+            db.save(payload)
+            return payload,201
+    
+    def read_user(self, user_id) -> str:
+        db = couch['login']
+        if(user_id in db):
+            user = db[user_id]
+            return user, 200
+        else:
+            return {"error": "User not found."}, 400
