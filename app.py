bcf0a4b4f4e47d0c637c559d9018e0d2bf14971f
LauraForde <G00315765@gmit.ie>
2018-04-27 13:43:22 +0100

    AH

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ import logging
 from connexion.resolver import RestyResolver
 from providers.CouchProvider import CouchProvider
 #from injector import Binder
+from connexion.apis import flask_utils
+from connexion.apis.abstract import AbstractAPI
+from connexion.decorators.produces import NoContent
+from connexion.handlers import AuthErrorHandler
+from connexion.lifecycle import ConnexionRequest, ConnexionResponse
+from connexion.utils import Jsonifier, is_json_mimetype
+
 logger = logging.getLogger('connexion.apis.app')
 
 app = connexion.App(__name__, specification_dir='swagger/')
