8cb916d50b6a662db6c15d43417e543e3607f916
LauraForde <G00315765@gmit.ie>
2018-04-27 13:58:56 +0100

    gjunfdk

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from providers.CouchProvider import CouchProvider
 #app.add_api('swagger.yaml', resolver=RestyResolver('providers'))
 
 if __name__ == '__main__':
-   # app = connexion.App(__name__, specification_dir='swagger/')  # Provide the app and the directory of the docs specification_dir='swagger/'
-    #app.add_api('swagger.yaml', arguments={'title': 'RestyResolver Example'}, resolver = RestyResolver('providers'))
+    app = connexion.App(__name__, specification_dir='swagger/')  # Provide the app and the directory of the docs specification_dir='swagger/'
+    app.add_api('swagger.yaml', arguments={'title': 'RestyResolver Example'}, resolver = RestyResolver('providers'))
     #FlaskInjector(app=app.app, modules=[configure])
     app.run(port = int(os.environ.get('POST', 2020))) 
 
