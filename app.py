e15c91e01164e81cf8e2201ccb0d19bc72873614
LauraForde <G00315765@gmit.ie>
2018-04-27 14:10:12 +0100

    trew

--- app.py
+++ app.py
@@ -6,10 +6,9 @@ from providers.CouchProvider import CouchProvider
 #from injector import Binder
 #app = connexion.App(__name__, specification_dir='swagger/')
 #app.add_api('swagger.yaml', resolver=RestyResolver('providers'))
-
-if __name__ == '__main__':
+if __name__ == '__name__':
     app = connexion.App(__name__, specification_dir='swagger/')  # Provide the app and the directory of the docs specification_dir='swagger/'
-    app.add_api('swagger.yaml', resolver = RestyResolver('providers'))
+    app.add_api('swagger.yaml', arguments={'title': 'RestyResolver Example'}, resolver = RestyResolver('providers'))
     #FlaskInjector(app=app.app, modules=[configure])
     app.run(port = int(os.environ.get('POST', 2020))) 
 
