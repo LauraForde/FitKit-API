5a94edfdbde9f92528521c0e3c807c63f29c4040
LauraForde <G00315765@gmit.ie>
2018-04-27 13:50:38 +0100

    AH

--- app.py
+++ app.py
@@ -1,19 +1,9 @@
 import os
 import connexion
-import logging
 #from flask_injector import FlaskInjector
 from connexion.resolver import RestyResolver
 from providers.CouchProvider import CouchProvider
 #from injector import Binder
-from connexion.apis import flask_utils
-from connexion.apis.abstract import AbstractAPI
-from connexion.decorators.produces import NoContent
-from connexion.handlers import AuthErrorHandler
-from connexion.lifecycle import ConnexionRequest, ConnexionResponse
-from connexion.utils import Jsonifier, is_json_mimetype
-
-logger = logging.getLogger('connexion.apis.app')
-
 app = connexion.App(__name__, specification_dir='swagger/')
 app.add_api('swagger.yaml', resolver=RestyResolver('providers'))
 
