bebf1dde0aa0479e3fb260f30df0fc7293a56bd7
LauraForde <G00315765@gmit.ie>
2018-04-27 13:34:12 +0100

    Adding things back in

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ import connexion
 from connexion.resolver import RestyResolver
 from providers.CouchProvider import CouchProvider
 #from injector import Binder
+app = connexion.App(__name__, specification_dir='swagger/')
+app.add_api('swagger.yaml', resolver=RestyResolver('providers'))
 
 if __name__ == '__main__':
-    app = connexion.App(__name__, specification_dir="swagger/")  # Provide the app and the directory of the docs specification_dir='swagger/'
+    app = connexion.App(__name__, specification_dir='swagger/')  # Provide the app and the directory of the docs specification_dir='swagger/'
     app.add_api('swagger.yaml', arguments={'title': 'RestyResolver Example'}, resolver = RestyResolver('providers'))
     #FlaskInjector(app=app.app, modules=[configure])
-    app.run(port = 2020) 
+    app.run(port = int(os.environ.get('POST', 2020))) 
 
 #specification_dir='swagger/'
