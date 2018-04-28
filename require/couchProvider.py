9f564b8e987620b9aa59f4a57980e93d18b8fabb
LauraForde <G00315765@gmit.ie>
2018-04-28 13:59:57 +0100

    y

--- require/couchProvider.py
+++ require/couchProvider.py
@@ -2,7 +2,7 @@ import requests
 import couchdb
 ADMIN_USERNAME = 'admin'
 ADMIN_PASSWORD = 'pass'
-COUCHDB_URL = 'http://54.68.14.217:5000/'
+COUCHDB_URL = 'http://54.68.14.217:5000/5984'
 #COUCHDB_URL = 'http://54.68.14.217:5984/'
 couch = couchdb.Server(os.environ['SERVER_URL'])
 
