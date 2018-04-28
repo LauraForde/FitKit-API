170454b6423fb0c561d7e77f4d4b0c9da09d68f0
LauraForde <G00315765@gmit.ie>
2018-04-28 12:48:58 +0100

    Adding windows instance ip to the url

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import connexion
-#from flask_injector import FlaskInjector
+from flask_cors import CORS
 from connexion.resolver import RestyResolver
 from providers.couchProvider import CouchProvider
 #from injector import Binder
@@ -8,14 +8,15 @@ from providers.couchProvider import CouchProvider
 #app.add_api('swagger.yaml', resolver=RestyResolver('providers'))
 import logging
 
-logger = logging.getLogger('connexion.apis.app')
+CORS(app.app)
 
+logger = logging.getLogger('connexion.apis.app')
+app.add_api('swagger.yaml', arguments={'title': 'RestyResolver Example'}, resolver = RestyResolver('providers'))
 
 if __name__ == '__main__':
     
     logger.debug('what')
     app = connexion.App(__name__, specification_dir='swagger/')  # Provide the app and the directory of the docs specification_dir='swagger/'
-    app.add_api('swagger.yaml', arguments={'title': 'RestyResolver Example'}, resolver = RestyResolver('providers'))
     #FlaskInjector(app=app.app, modules=[configure])
     app.run(port = int(os.environ.get('POST', 5000))) 
 
