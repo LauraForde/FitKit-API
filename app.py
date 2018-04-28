1248f8736a632824df0b9426083bc3382e5a44fd
LauraForde <G00315765@gmit.ie>
2018-04-28 15:59:04 +0100

    Still trying to connect

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 import connexion
-import flask as fl
 from connexion.resolver import RestyResolver
 from providers.couchProvider import CouchProvider
 #from injector import Binder
@@ -8,15 +7,10 @@ from providers.couchProvider import CouchProvider
 #app.add_api('swagger.yaml', resolver=RestyResolver('providers'))
 import logging
 
-app = fl(__name__)
-CORS(app)
-
 logger = logging.getLogger('connexion.apis.app')
 app.add_api('swagger.yaml', arguments={'title': 'RestyResolver Example'}, resolver = RestyResolver('providers'))
 
 if __name__ == '__main__':
-    
-    logging.getLogger('flask_cors').level = logging.DEBUG
     app = connexion.App(__name__, specification_dir='swagger/')  # Provide the app and the directory of the docs specification_dir='swagger/'
     #FlaskInjector(app=app.app, modules=[configure])
     app.run(debug = True, port = int(os.environ.get('POST', 5000))) 
