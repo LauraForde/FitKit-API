6cd6d90ceab86e984a9f7a3860c1008c92e2b211
LauraForde <G00315765@gmit.ie>
2018-04-28 14:52:52 +0100

    Setting debug to true

--- require/couchProvider.py
+++ require/couchProvider.py
@@ -16,7 +16,7 @@ class CouchProvider(object):
             db.save(payload)
             return payload,201
     
-    def read_user(self, user_id):
+    def read_user(self):
         db = couch['login']
         if(user_id in db):
             user = db[user_id]
