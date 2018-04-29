9a36cd0f5a431ce730a5a2f08e17f95d387591f1
LauraForde <G00315765@gmit.ie>
2018-04-29 09:01:36 +0100

    Changed few things

--- app.py
+++ app.py
@@ -12,12 +12,13 @@ app = Flask(__name__)
 CORS(app)
 
 logger = logging.getLogger('connexion.apis.app')
-app.add_api('swagger.yaml', arguments={'title': 'RestyResolver Example'}, resolver = RestyResolver('providers'))
-    
+   
 
 if __name__ == '__main__':
     
     logger.debug('what')
-   # app = connexion.App(__name__, specification_dir='swagger/')  # Provide the app and the directory of the docs specification_dir='swagger/'
+    app.add_api('swagger.yaml', arguments={'title': 'RestyResolver Example'}, resolver = RestyResolver('providers'))
+ 
+    app = connexion.App(__name__, specification_dir='swagger/')  # Provide the app and the directory of the docs specification_dir='swagger/'
     #FlaskInjector(app=app.app, modules=[configure])
     app.run(port = int(os.environ.get('POST', 5000)))
